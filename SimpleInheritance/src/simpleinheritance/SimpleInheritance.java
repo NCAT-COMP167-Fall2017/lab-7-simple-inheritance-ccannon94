7937566c55c046b22f00ca17c1937f61ee1764e1
Chris Cannon <chris.c.cannon@gmail.com>
2017-10-18 21:10:18 -0400

    Implement readHouse

--- SimpleInheritance/src/simpleinheritance/SimpleInheritance.java
+++ SimpleInheritance/src/simpleinheritance/SimpleInheritance.java
@@ -5,17 +5,81 @@
  */
 package simpleinheritance;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Scanner;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 /**
  *
  * @author CCannon
  */
 public class SimpleInheritance {
-
+    private static Scanner reader;
+    
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        ArrayList<Dwelling> dwellings = new ArrayList<>();
+        
+        try {
+            reader = new Scanner(new File(args[0]));
+            
+            char dwellingType = reader.nextLine().charAt(0);
+            
+            switch(dwellingType) {
+                case 'H':
+                    dwellings.add(readApartment());
+                    break;
+                case 'A':
+                    dwellings.add(readHouse());
+                    break;
+            }
+        } catch (FileNotFoundException ex) {
+            Logger.getLogger(SimpleInheritance.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+    
+    public static Apartment readApartment() {
+        Apartment apartment = new Apartment();
+        
+        apartment.setStreetAddress(reader.nextLine());
+        apartment.setCity(reader.nextLine());
+        apartment.setState(reader.nextLine());
+        apartment.setZip(reader.nextLine());
+        apartment.setBedrooms(Integer.parseInt(reader.nextLine().trim()));
+        apartment.setBathrooms(Double.parseDouble(reader.nextLine().trim()));
+        apartment.setAptNum(reader.nextLine());
+        int laundryInt = Integer.parseInt(reader.nextLine().trim());
+        
+        switch(laundryInt) {
+            case 0:
+                apartment.setLaundry(false);
+                break;
+            case 1:
+                apartment.setLaundry(true);
+                break;
+        }
+        
+        return apartment;
+    }
+    
+    public static House readHouse() {
+        House house = new House();
+        
+        house.setStreetAddress(reader.nextLine());
+        house.setCity(reader.nextLine());
+        house.setState(reader.nextLine());
+        house.setZip(reader.nextLine());
+        house.setBedrooms(Integer.parseInt(reader.nextLine().trim()));
+        house.setBathrooms(Double.parseDouble(reader.nextLine().trim()));
+        house.setAcreage(Double.parseDouble(reader.nextLine().trim()));
+        house.setGarageSize(Integer.parseInt(reader.nextLine().trim()));
+        
+        return house;
     }
     
 }
